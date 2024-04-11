76827fca1e938558195806f1795b1b6abb01ce87
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-11 16:09:32 +0200

    feat(heroes): :sparkles: add new.page form

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``
 })
 export class NewPageComponent {
-
+  publishers = [
+    {id: 'DC Comics', desc: 'DC - Comics'},
+    {id: 'Marvel Comics', desc: 'Marvel - Comics'}
+  ];
 }
