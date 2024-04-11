9c067d8a4a6f35b0345ddcba49840f15da2eb9de
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-11 15:56:58 +0200

    feat(heroes): :sparkles: implemets search Page functionability

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class SearchPageComponent {
+  searchInput = new FormControl('');
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
 
+  constructor(private heroService:HeroesService) {}
+  searchHero() {
+    const value:string = this.searchInput.value || '';
+
+    if (value.trim().length === 0) {
+      return;
+    }
+
+    this.heroService.getSuggestions(value)
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelectedOption(event:MatAutocompleteSelectedEvent) {
+    if(!event.option.value) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero:Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+
+    this.heroService.getHeroById(hero.id)
+  }
 }
