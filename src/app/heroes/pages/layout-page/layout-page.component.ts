9953e8e5953a408a7f72d8a4cc292191d963d12f
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-12 14:21:22 +0200

    fix(auth): :sparkles: add auth service and localStorage data for loggin and logout

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,14 +1,29 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthModule } from '../../../auth/auth.module';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
-	selector: 'app-layout-page',
-	templateUrl: './layout-page.component.html',
-	styles: '',
+  selector: 'app-layout-page',
+  templateUrl: './layout-page.component.html',
+  styles: '',
 })
 export class LayoutPageComponent {
-	sidebarItems = [
-		{label: 'Listado', icon: 'list', url: './list'},
-		{label: 'Añadir', icon: 'add', url: './new-hero'},
-		{label: 'Buscar', icon: 'search', url: './search'},
-	];
+  sidebarItems = [
+    { label: 'Listado', icon: 'list', url: './list' },
+    { label: 'Añadir', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ];
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  get user(): User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth']);
+  }
 }
