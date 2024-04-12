b98517eb09c4c43fd600cf6e30401cc642748a00
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-12 10:33:04 +0200

    feat(heroes): :sparkles: Add reactive form

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 
 @Component({
@@ -7,8 +10,37 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class NewPageComponent {
+
+  heroForm = new FormGroup({
+    id:  new FormControl(''),
+    superhero:  new FormControl('',{}),
+    publisher:  new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego:  new FormControl(''),
+    first_appearance:  new FormControl(''),
+    characters:  new FormControl(''),
+    alt_img: new FormControl('')
+  });
+
   publishers = [
     {id: 'DC Comics', desc: 'DC - Comics'},
     {id: 'Marvel Comics', desc: 'Marvel - Comics'}
   ];
+
+  constructor( private heroesService: HeroesService ) {}
+
+  get currentHero():Hero{
+    return this.heroForm.value as Hero;
+  }
+
+  onSubmit():void{
+    if(this.heroForm.invalid){
+      this.heroForm.markAllAsTouched();
+      return;
+    }
+
+    // this.heroesService.addHero()
+    //   .subscribe( hero => {
+    //     console.log('Saved', hero);
+    //   });
+  }
 }
