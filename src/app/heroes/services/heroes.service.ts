181229e3145a729a768d56e904f1b5ff06db784f
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-12 09:27:38 +0200

    feat(heroes): :sparkles: Add service enpoints

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environment } from '../../../environments/environments';
 
@@ -24,5 +24,21 @@ export class HeroesService {
 
   getSuggestions(query:string):Observable<Hero[]>{
     return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero:Hero):Observable<Hero>{
+    return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`,hero);
+  }
+
+  update(hero:Hero):Observable<Hero>{
+    if(!hero.id) throw new Error('Hero must have an id');
+    return this.httpClient.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`,hero);
+  }
+
+  deleteHero(id:string):Observable<boolean>{
+    return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError( err => of(false)),
+      map( () => true)
+    )
   }
 }
